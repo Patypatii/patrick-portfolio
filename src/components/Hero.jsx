c3f96bc7ea8899f134ba4a33402e6bf530a334d5
Patypatii <patrickwambugu707@gmail.com>
2025-07-06 09:33:06 +0300

    initial commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,13 +15,13 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className="text-[#915EFF]">lohit</span>
+            Hi, I'm <span className="text-[#915EFF]">Patrick</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I do
             <Typewriter
               options={{
-                strings: ["CyberSecurity", "Ai & ML", "NLP"],
+                strings: ["Web Applications", "Mobile Development", "UI/UX Design", "Software Development"],
                 autoStart: true,
                 loop: true,
                 loopCount: Infinity,
